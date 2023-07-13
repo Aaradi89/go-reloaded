368c2415f745c117844535346d1ba726c04a46bb
AliAlaradi0 <dragonsmaster@live.com>
2023-07-13 17:10:17 +0300

    6

--- inputAndOutput.go
+++ inputAndOutput.go
@@ -3,26 +3,39 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"os"
 )
 
-func getInput() (string, bool) {
+func getInput() ([]string, bool) {
 	inputFile := os.Args[1]
 	file, err := os.Open(inputFile)
 	if err != nil {
 		fmt.Println("Input file cannot be found")
-		return "", false
+		return nil, false
 	}
 	defer file.Close()
-	content, _ := io.ReadAll(file)
-	return string(content), true
+	// content, _ := io.ReadAll(file)
+	// return string(content), true
+	scan := bufio.NewScanner(file)
+	var text []string
+	for scan.Scan() {
+		text = append(text, scan.Text())
+	}
+	return text, true
 }
 
-func sendOutput(text string) {
+func sendOutput(text []string) {
 	outputFile := os.Args[2]
 	file, _ := os.Create(outputFile)
+	defer file.Close()
 	writer := bufio.NewWriter(file)
-	writer.WriteString(text)
+	//writer.WriteString(text)
+	for i, line := range text {
+		if i < len(text)-1 {
+			fmt.Fprintln(writer, line)
+		} else {
+			fmt.Fprint(writer, line)
+		}
+	}
 	writer.Flush()
 }
--- main.go
+++ main.go
@@ -3,28 +3,33 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
 	if len(os.Args[1:]) == 2 {
-		text, correct := getInput()
+		inputText, correct := getInput()
 		if correct {
-			textArry := SplitInput(text)
-			text = strings.Join(textArry, " ")
-			text = hex(text)
-			text = bin(text)
-			text = up(text)
-			text = low(text)
-			text = cap(text)
-			text = specialUp(text)
-			text = specialLow(text)
-			text = specialCap(text)
-			text = punctuations(text)
-			text = fixSingleQuotations(text)
-			text = fixDoubleQuotations(text)
-			text = convertA(text)
-			sendOutput(text)
+			// textArry := SplitInput(text)
+			// text = strings.Join(textArry, " ")
+			var outputText []string
+			for _, line := range inputText {
+				text := line
+				text = fixSpaces(text)
+				text = hex(text)
+				text = bin(text)
+				text = up(text)
+				text = low(text)
+				text = cap(text)
+				text = specialUp(text)
+				text = specialLow(text)
+				text = specialCap(text)
+				text = punctuations(text)
+				text = fixSingleQuotations(text)
+				text = fixDoubleQuotations(text)
+				text = convertA(text)
+				outputText = append(outputText, text)
+			}
+			sendOutput(outputText)
 			fmt.Println("Completed")
 		}
 	} else if len(os.Args[1:]) < 2 {
